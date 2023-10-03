150ea73fe93f2ac08d18f0ae564c791d61540d7b
Cledson do Nascimento Pereira <142537029+DevCled85@users.noreply.github.com>
2023-10-03 12:10:13 -0400

    updates components dinamics properts

--- src/app/components/smal-card/smal-card.component.ts
+++ src/app/components/smal-card/smal-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-smal-card',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmalCardComponent implements OnInit {
 
+  @Input()
+  photoCover:string = ""
+
+  @Input()
+  cardDescription:string = ""
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
+
