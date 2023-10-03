9b5601793265f6292491f219033642a86166c7bc
Cledson do Nascimento Pereira <142537029+DevCled85@users.noreply.github.com>
2023-10-03 01:50:47 -0400

    components createds whith styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmalCardComponent } from './components/smal-card/smal-card.component';
+import { NewsCardComponent } from './components/news-card/news-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmalCardComponent,
+    NewsCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/smal-card/smal-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-smal-card',
+  templateUrl: './smal-card.component.html',
+  styleUrls: ['./smal-card.component.css']
+})
+export class SmalCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
