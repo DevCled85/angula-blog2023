29843ec9efce780e41104664e8b42143de067a2b
Cledson do Nascimento Pereira <142537029+DevCled85@users.noreply.github.com>
2023-10-03 02:20:47 -0400

    add styles responsive

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
-  styleUrls: ['./menu-title.component.css']
+  styleUrls: [
+    './menu-title.component.css',
+    './responsivo-menu-title.component.css'
+  ]
 })
 export class MenuTitleComponent implements OnInit {
 
